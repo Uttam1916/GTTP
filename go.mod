852ca027f71024aae3c9d714c2451c1a37b9a739
Uttam1916 <tired3120@gmail.com>
2025-10-04 16:58:50 +0530

    learnt how to read a file byte wise

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Uttam1916/GTTP
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io"
+	)
+
+func main(){
+	f, err := os.Open("messages.txt")
+	if err != nil{
+		fmt.Println(err)
+	}
+	defer f.Close()
+	
+	buffer := make([]byte, 8)
+
+	for{
+		_, err := f.Read(buffer)
+		if err == io.EOF{
+			fmt.Println(err)
+			break
+		}
+		if err != nil{
+			fmt.Println(err)
+			break
+		}
+		fmt.Printf("%s \n",buffer)
+	}
+}
